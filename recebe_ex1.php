617179ed208adfe839fe02aa69bc127c53b8f4ea
maurojh <maurojh@gmail.com>
2019-08-06 20:10:42 -0300

    exercicio 3 pronto

---
+++ recebe_ex1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Recebe</title>
+</head>
+<body>
+<?php
+    $recebido = $_GET['senha'];
+    echo $recebido;
+</body>
+</html>
